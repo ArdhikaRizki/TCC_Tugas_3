5ab5f1bdf79edd0705cd88e56fe30889ffba1f9a
rizki <ardhikaz30@gmail.com>
2026-05-10 20:43:59 +0700

    add a migrations

---
+++ migrations/20260510130954-create-note.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('notes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      judul: {
+        type: Sequelize.STRING(150),
+        allowNull: false
+      },
+      isi: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+      tanggal_dibuat: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('notes');
+  }
+};
